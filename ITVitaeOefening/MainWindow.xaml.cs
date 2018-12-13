7e9add3518c952c2d807af0366b09aa0c62e8da6
Huego <hugozaanen@hotmail.com>
2018-12-13 13:59:01 +0100

    ItVitea oefening

--- ITVitaeOefening/MainWindow.xaml.cs
+++ ITVitaeOefening/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace ITVitaeOefening
         {
             InitializeComponent();
         }
+
+        private void Berekenen(object sender, RoutedEventArgs e)
+        {   int x;
+            if (int.TryParse(Getal1.Text, out x))
+                return;
+            int getal1 = int.Parse(Getal1.Text);
+            if (int.TryParse(Getal1.Text, out x))
+                return;
+            int getal2 = int.Parse(Getal2.Text);
+
+            int antwoord = 0;
+            
+            if((bool)Optellen.IsChecked)
+            {
+                antwoord = Int32.Parse(Getal1.Text) + Int32.Parse(Getal2.Text);
+                Veld.Content = $"{getal1} + {getal2} = {antwoord}";
+                History.Items.Add($"{getal1} + {getal2} = {antwoord}");
+            }
+            else if((bool)Aftrekken.IsChecked)
+            {
+                antwoord = Int32.Parse(Getal1.Text) - Int32.Parse(Getal2.Text);
+                Veld.Content = $"{getal1} - {getal2} = {antwoord}";
+                History.Items.Add($"{getal1} - {getal2} = {antwoord}");
+            }
+            else if((bool)Vermenigvuldigen.IsChecked)
+            {
+                antwoord = Int32.Parse(Getal1.Text) * Int32.Parse(Getal2.Text);
+                Veld.Content = $"{getal1} * {getal2} = {antwoord}";
+                History.Items.Add($"{getal1} * {getal2} = {antwoord}");
+            }
+            else if((bool)Delen.IsChecked)
+            {
+                antwoord = Int32.Parse(Getal1.Text) / Int32.Parse(Getal2.Text);
+                Veld.Content = $"{getal1} / {getal2} = {antwoord}";
+                History.Items.Add($"{getal1} / {getal2} = {antwoord}");
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            History.Items.Clear();
+        }
     }
 }
